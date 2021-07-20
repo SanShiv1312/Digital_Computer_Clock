9e70959a9ef7c0b7a20b362eacbaa4d43a64ad33
CHINDUKURU SAI SANDEEP <76940846+SanShiv1312@users.noreply.github.com>
2021-07-20 22:20:17 +0530

    Update Digital_Computer_Clock.py

--- Digital_Computer_Clock.py
+++ Digital_Computer_Clock.py
@@ -4,7 +4,7 @@ from time import strftime
 root = Tk()
 root.title("Digital Computer Clock")
 
-fgcolor = ["aqua", "green", "blue", "yellow", "red", "pink", "purple", "magenta"]
+fgcolor = ["aqua", "green", "blue", "yellow", "pink", "red", "purple", "magenta"]
 
 
 def Time(counter=0):
